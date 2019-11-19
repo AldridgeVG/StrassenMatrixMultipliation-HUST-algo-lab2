76482b7bc28b82ba04f11a9525cbd96709edda57
aldridgevg <caxuser@live.com>
2019-11-19 08:02:33 +0800

    adiing function of chain mul

--- src/matrixMultipliation.kt
+++ src/matrixMultipliation.kt
@@ -17,6 +17,24 @@ class Matrix(val n: Int) {
     }
 }
 
+class Matrix1(val n: Int, val m: Int) {
+    //if n=0 or m=0, initialization failure
+    init {
+        require(n > 0 && m > 0) { println("invalid matrix initialization") }
+    }
+
+    //initialize n*n Integer array elem
+    var elem = Array(n) { IntArray(m) { 0 } }
+
+    fun getLineSize(): Int {
+        return n
+    }
+
+    fun getRollSize(): Int {
+        return m
+    }
+}
+
 //CONSTRUCT SUB MATRIX FROM EXISTING MATRIX-----------------------------------//
 //CONSTRUCT MATRIX FROM EXISTING SUB MATRIX
 fun cutSubMatrix(A: Matrix, i: Int, j: Int, n: Int): Matrix {
@@ -112,7 +130,34 @@ fun matrixMultiplication(A: Matrix, B: Matrix): Matrix {
         i++
     }
     return C
-}   //---checked
+}
+
+fun matrix1Multiplication(A: Matrix1, B: Matrix1): Matrix1 {
+    val an = A.getLineSize()
+    val am = A.getRollSize()
+    val bn = B.getLineSize()
+    val bm = B.getRollSize()
+    if (am != bn) {
+        println("Matrix size doesn't fit")
+    }
+    val C = Matrix1(an, bm)
+    var i = 0
+    var j = 0
+    var k = 0
+    while (i < an) {
+        j = 0
+        while (j < bm) {
+            k = 0
+            while (k < am) {
+                C.elem[i][j] += A.elem[i][k] * B.elem[k][j]
+                k++
+            }
+            j++
+        }
+        i++
+    }
+    return C
+}
 
 fun matrixAdd(A: Matrix, B: Matrix): Matrix {
     if (A.getSize() != B.getSize()) {
@@ -178,6 +223,31 @@ fun readMatrix(filename: String): Matrix {
     return M
 }
 
+fun readMatrix1(filename: String): Matrix1 {
+    val line = File(filename).readLines()
+    val n = line[0].toInt()
+    val m = line[1].toInt()
+    val readElem = line[2].split(" ").toList().toTypedArray()
+    if (readElem.size != n * m) {
+        println("file reading error")
+        return Matrix1(0, 0)
+    }
+    var M = Matrix1(n, m)
+    var i = 0
+    var j = 0
+    var k = 0
+    while (i < n) {
+        j = 0
+        while (j < m) {
+            M.elem[i][j] = readElem[k].toInt()
+            j++
+            k++
+        }
+        i++
+    }
+    return M
+}
+
 fun printMatrix(M: Matrix) {
     val n = M.getSize()
     if (n <= 0) {
@@ -199,91 +269,178 @@ fun printMatrix(M: Matrix) {
     println()
 }
 
-//-----------------------------------------------------------------------------//
-fun main(args: Array<String>) {
-    //file read
-    println("input filename of matrix M:")
-    val filename1 = readLine()!!
-    println("input filename of matrix N:")
-    val filename2 = readLine()!!
-    val M = readMatrix(filename1)
-    val N = readMatrix(filename2)
-    var n = 0
-    if (M.getSize() == N.getSize()) {
-        n = M.getSize()
-    } else {
-        println("file reading error")
+fun printMatrix1(M: Matrix1) {
+    val n = M.getLineSize()
+    val m = M.getRollSize()
+    if (n <= 0) {
+        println("invalid matrix input!")
         return
     }
-    val m = n / 2
-    println("Matrix for Multiplication:")
-    println("Matrix M:")
-    printMatrix(M)
-    println("Matrix N:")
-    printMatrix(N)
+    println("matrix size: n = $n\n")
+    var i = 0
+    var j = 0
+    while (i < n) {
+        j = 0
+        while (j < m) {
+            print("${M.elem[i][j]} \t\t")
+            j++
+        }
+        i++
+        println("\n\n")
+    }
+    println()
+}
 
-    //naive matrix multi answer for comparison----------------------------//
-    val startTime = System.nanoTime()
-    val ans = matrixMultiplication(M, N)
-    val endTime = System.nanoTime()
-    println("naive ANSWER:")
-    printMatrix(ans)
-    //-----------------------------------------------------------------//
+fun readMatrices(n: Int): Array<Matrix1> {
+    var i = 0
+    var m = Array(n) { Matrix1(0, 0) }
+    var filename: String
+    while (i < n) {
+        println("Input filename of Matrix $i:")
+        filename = readLine()!!
+        m[i] = readMatrix1(filename)
+        i++
+    }
+    return m
+}
 
-    //construct Strassen factor --- checked
-    val A = cutSubMatrix(M, 0, 0, m)
-    val B = cutSubMatrix(M, 0, m, m)
-    val C = cutSubMatrix(M, m, 0, m)
-    val D = cutSubMatrix(M, m, m, m)
+//-----------------------------------------------------------------------------//
+fun main(args: Array<String>) {
+    var op = 3
+    //select op, input controll
+    while (op != 1 || op != 2 || op != 0) {
+        println("select op (0 to quit):")
+        println("1: Strassen's Matrix Multiplication test")
+        println("2: Matrices Chain Multiplication test")
+        op = readLine()!!.toInt()
+    }
+    if (op == 1) {
+        println("1. Strassen's Matrix Multiplication:")
+        //file read
+        println("input filename of matrix M:")
+        val filename1 = readLine()!!
+        println("input filename of matrix N:")
+        val filename2 = readLine()!!
+        val M = readMatrix(filename1)
+        val N = readMatrix(filename2)
+        var n = 0
+        if (M.getSize() == N.getSize()) {
+            n = M.getSize()
+        } else {
+            println("file reading error")
+            return
+        }
+        val m = n / 2
+        println("Matrix for Multiplication:")
+        println("Matrix M:")
+        printMatrix(M)
+        println("Matrix N:")
+        printMatrix(N)
 
-    val E = cutSubMatrix(N, 0, 0, m)
-    val F = cutSubMatrix(N, 0, m, m)
-    val G = cutSubMatrix(N, m, 0, m)
-    val H = cutSubMatrix(N, m, m, m)
+        //naive matrix multi answer for comparison----------------------------//
+        val startTime = System.nanoTime()
+        val ans = matrixMultiplication(M, N)
+        val endTime = System.nanoTime()
+        println("naive ANSWER:")
+        printMatrix(ans)
+        //-----------------------------------------------------------------//
 
-    //naive D&V multi answer----------------------------------------------------------------------------//
-    val startTime1 = System.nanoTime()
-    val NorthWest1 = matrixAdd(matrixMultiplication(A, E), matrixMultiplication(B, G))   //NorthWest = ae+bg
-    val NorthEast1 = matrixAdd(matrixMultiplication(A, F), matrixMultiplication(B, F))   //NorthEast = af+bh
-    val SouthWest1 = matrixAdd(matrixMultiplication(C, E), matrixMultiplication(D, G))   //SouthWest = ce+dg
-    val SouthEast1 = matrixAdd(matrixMultiplication(C, F), matrixMultiplication(D, H))   //SouthEast = cf+dh
-    val endTime1 = System.nanoTime()
-    val result1 = formMatrix(NorthWest1, NorthEast1, SouthWest1, SouthEast1)
-    println("naive D&V MM ANSWER:")
-    printMatrix(result1)
-    //--------------------------------------------------------------------------------------------------//
+        //construct Strassen factor --- checked
+        val A = cutSubMatrix(M, 0, 0, m)
+        val B = cutSubMatrix(M, 0, m, m)
+        val C = cutSubMatrix(M, m, 0, m)
+        val D = cutSubMatrix(M, m, m, m)
+
+        val E = cutSubMatrix(N, 0, 0, m)
+        val F = cutSubMatrix(N, 0, m, m)
+        val G = cutSubMatrix(N, m, 0, m)
+        val H = cutSubMatrix(N, m, m, m)
+
+        //naive D&V multi answer----------------------------------------------------------------------------//
+        val startTime1 = System.nanoTime()
+        val NorthWest1 = matrixAdd(matrixMultiplication(A, E), matrixMultiplication(B, G))   //NorthWest = ae+bg
+        val NorthEast1 = matrixAdd(matrixMultiplication(A, F), matrixMultiplication(B, F))   //NorthEast = af+bh
+        val SouthWest1 = matrixAdd(matrixMultiplication(C, E), matrixMultiplication(D, G))   //SouthWest = ce+dg
+        val SouthEast1 = matrixAdd(matrixMultiplication(C, F), matrixMultiplication(D, H))   //SouthEast = cf+dh
+        val endTime1 = System.nanoTime()
+        val result1 = formMatrix(NorthWest1, NorthEast1, SouthWest1, SouthEast1)
+        println("naive D&V MM ANSWER:")
+        printMatrix(result1)
+        //--------------------------------------------------------------------------------------------------//
 
 
-    //Strassen's Matrix Multiplication------------------------------------------------------//
-    //Step1 form factor 1-7
-    val startTime2 = System.nanoTime()
-    val P1 = matrixMultiplication(A, matrixMinus(F, H))                  //p1 = a(f-h)
-    val P2 = matrixMultiplication(matrixAdd(A, B), H)                    //p2 = (a+b)h
-    val P3 = matrixMultiplication(matrixAdd(C, D), E)                    //p3 = (c+d)e
-    val P4 = matrixMultiplication(D, matrixMinus(G, E))                  //p4 = d(g-e)
-    val P5 = matrixMultiplication(matrixAdd(A, D), matrixAdd(E, H))      //p5 = (a+d)(e+h)
-    val P6 = matrixMultiplication(matrixMinus(B, D), matrixAdd(G, H))    //p6 = (b-d)(g+h)
-    val P7 = matrixMultiplication(matrixMinus(A, C), matrixAdd(E, F))    //p7 = (a-c)(e+f)
+        //Strassen's Matrix Multiplication------------------------------------------------------//
+        //Step1 form factor 1-7
+        val startTime2 = System.nanoTime()
+        val P1 = matrixMultiplication(A, matrixMinus(F, H))                  //p1 = a(f-h)
+        val P2 = matrixMultiplication(matrixAdd(A, B), H)                    //p2 = (a+b)h
+        val P3 = matrixMultiplication(matrixAdd(C, D), E)                    //p3 = (c+d)e
+        val P4 = matrixMultiplication(D, matrixMinus(G, E))                  //p4 = d(g-e)
+        val P5 = matrixMultiplication(matrixAdd(A, D), matrixAdd(E, H))      //p5 = (a+d)(e+h)
+        val P6 = matrixMultiplication(matrixMinus(B, D), matrixAdd(G, H))    //p6 = (b-d)(g+h)
+        val P7 = matrixMultiplication(matrixMinus(A, C), matrixAdd(E, F))    //p7 = (a-c)(e+f)
 
-    //Step2 form 4 sub matrix
-    val NorthWest = matrixAdd(matrixAdd(P5, P6), matrixMinus(P4, P2))       //NW = P5+P4-P2+P6
-    val NorthEast = matrixAdd(P1, P2)                                       //NE = P1+P2
-    val SouthWest = matrixAdd(P3, P4)                                       //SW = P3+P4
-    val SouthEast = matrixAdd(matrixMinus(P1, P3), matrixMinus(P5, P7))     //SE = P1+P5-P3-P7
-    val endTime2 = System.nanoTime()
+        //Step2 form 4 sub matrix
+        val NorthWest = matrixAdd(matrixAdd(P5, P6), matrixMinus(P4, P2))       //NW = P5+P4-P2+P6
+        val NorthEast = matrixAdd(P1, P2)                                       //NE = P1+P2
+        val SouthWest = matrixAdd(P3, P4)                                       //SW = P3+P4
+        val SouthEast = matrixAdd(matrixMinus(P1, P3), matrixMinus(P5, P7))     //SE = P1+P5-P3-P7
+        val endTime2 = System.nanoTime()
 
-    //Step3 form result matrix
-    val result = formMatrix(NorthWest, NorthEast, SouthWest, SouthEast)
-    //-------------------------------------------------------------------------------------//
+        //Step3 form result matrix
+        val result = formMatrix(NorthWest, NorthEast, SouthWest, SouthEast)
+        //-------------------------------------------------------------------------------------//
 
-    println("Matrix RESULT:")
-    printMatrix(result)
+        println("Matrix RESULT:")
+        printMatrix(result)
 
-    println("for matrix size n = $n:\n")
-    println("naive method time:        " + (endTime - startTime) + "ns")
-    println("naive D&V MM method time: " + (endTime1 - startTime1) + "ns")
-    println("SMM method time:          " + (endTime2 - startTime2) + "ns")
-    println("SMM algo is ${(endTime - startTime) - (endTime2 - startTime2)}ns(${((endTime - startTime) - (endTime2 - startTime2)) / 100000000.00}s) faster than naive MM algo")
-    println("SMM algo is ${(endTime1 - startTime1) - (endTime2 - startTime2)}ns(${((endTime1 - startTime1) - (endTime2 - startTime2)) / 1000000000.0}s) faster than normal D&V MM algo")
+        println("for matrix size n = $n:\n")
+        println("naive method time:        " + (endTime - startTime) + "ns")
+        println("naive D&V MM method time: " + (endTime1 - startTime1) + "ns")
+        println("SMM method time:          " + (endTime2 - startTime2) + "ns")
+        println("SMM algo is ${(endTime - startTime) - (endTime2 - startTime2)}ns(${((endTime - startTime) - (endTime2 - startTime2)) / 100000000.00}s) faster than naive MM algo")
+        println("SMM algo is ${(endTime1 - startTime1) - (endTime2 - startTime2)}ns(${((endTime1 - startTime1) - (endTime2 - startTime2)) / 1000000000.0}s) faster than normal D&V MM algo")
 //    println("SMM algo is %${((endTime - startTime)- (endTime1 - startTime1))/(endTime - startTime)*100} faster than normal D&V MM algo")
+    }
+    if (op == 2) {
+        println("2. Matrices Chain Multiplication:")
+        println("Input a number n(>1) signifying number of matrices:")
+        val N = readLine()!!.toInt()
+        val matrixChain = readMatrices(N)
+        //this chain's matrices' m,n must satisfy: m,n n,o o,p p,q... forms m,q
+        //check multiplication validity
+        var i = 0
+        while (i < N - 1) {
+            if (matrixChain[i].getRollSize() != matrixChain[i + 1].getLineSize()) {
+                println("matrices read cannot be multiplied!")
+                return
+            } else i++
+        }
+
+        val n = matrixChain[0].getLineSize()
+        val m = matrixChain[N - 1].getRollSize()
+        var result = Matrix1(n, m)
+
+        //there're N-1 multiplication in total, have to decide order
+        var price = Array(N) { IntArray(N) { Int.MAX_VALUE } }
+        var interval = 1
+        while (interval < N) {
+            i = 0
+            while (i < N - interval) {
+                if (interval == 1) {
+                    price[i][i + interval] =
+                        matrixChain[i].getLineSize() * matrixChain[i].getRollSize() * matrixChain[i + interval].getRollSize()
+                } else{
+                    //adding function to get the minnium times of mul of matrix chain multiplication
+                }
+
+                    i++
+            }
+            interval++
+        }
+    }
+    if (op == 0) {
+        println("system quit")
+        return
+    }
 }
